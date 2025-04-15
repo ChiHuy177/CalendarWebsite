cefe7f30403b9c95edd075f31a9851e7238f491a
ChiHuy177 <huynguyenbd54@gmail.com>
2025-04-15 14:46:55 +0700

    sửa certificate

--- CalendarWebsite/calendarwebsite.client/vite.config.ts
+++ CalendarWebsite/calendarwebsite.client/vite.config.ts
@@ -16,27 +16,28 @@ const certificateName = "calendarwebsite.client";
 const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-if (!fs.existsSync(baseFolder)) {
-    fs.mkdirSync(baseFolder, { recursive: true });
-}
+// Chỉ tạo chứng chỉ trong môi trường development
+if (process.env.NODE_ENV === 'development') {
+    if (!fs.existsSync(baseFolder)) {
+        fs.mkdirSync(baseFolder, { recursive: true });
+    }
 
-if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-    if (0 !== child_process.spawnSync('dotnet', [
-        'dev-certs',
-        'https',
-        '--export-path',
-        certFilePath,
-        '--format',
-        'Pem',
-        '--no-password',
-    ], { stdio: 'inherit', }).status) {
-        throw new Error("Could not create certificate.");
+    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+        if (0 !== child_process.spawnSync('dotnet', [
+            'dev-certs',
+            'https',
+            '--export-path',
+            certFilePath,
+            '--format',
+            'Pem',
+            '--no-password',
+        ], { stdio: 'inherit', }).status) {
+            throw new Error("Could not create certificate.");
+        }
     }
 }
 
-const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7179';
-const target1 = 'https://calendarwebsite-my2z.onrender.com'; // URL backend từ Render
+const target = 'https://calendarwebsite-my2z.onrender.com'; // URL backend từ Render
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin(), tailwindcss()],
@@ -52,14 +53,11 @@ export default defineConfig({
         https: process.env.NODE_ENV === 'development' ? {
             key: fs.readFileSync('./path/to/localhost-key.pem'),
             cert: fs.readFileSync('./path/to/localhost-cert.pem'),
-        } : false,
+        } : undefined,
         proxy: {
-            '^/weatherforecast': {
-                target,
-                secure: false
-            },
+            
             '^/api': {
-                target: target1,
+                target,
                 secure: false,
                 changeOrigin: true,
             },
