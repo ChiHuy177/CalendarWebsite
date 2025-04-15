3dac73013806f6dd530b36cd45d163d055e5974c
ChiHuy177 <huynguyenbd54@gmail.com>
2025-04-15 14:31:12 +0700

    Deploy backend

--- CalendarWebsite/calendarwebsite.client/vite.config.ts
+++ CalendarWebsite/calendarwebsite.client/vite.config.ts
@@ -36,7 +36,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
     env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7179';
-
+const target1 = 'https://calendarwebsite-my2z.onrender.com'; // URL backend từ Render
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin(), tailwindcss()],
@@ -53,7 +53,12 @@ export default defineConfig({
             '^/weatherforecast': {
                 target,
                 secure: false
-            }
+            },
+            '^/api': {
+                target: target1,
+                secure: false,
+                changeOrigin: true,
+            },
         },
         port: 50857,
         https: {
